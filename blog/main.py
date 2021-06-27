36602bcafc4251ea336ffa2db44a75d699ca872f
RojasxAndres <afrojaslondono@gmail.com>
2021-06-27 16:19:32 -0500

    Creando blog

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from blog.schemas import BlogValidate
 from blog.models import *
-from blog.database import engine
-
+from blog.database import engine,SessionLocal
+from sqlalchemy.orm import Session
 app = FastAPI()
+
 Base.metadata.create_all(bind=engine) # Esta linea crea todas las tablas en la base de datos al iniciar la app
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  
+    finally:
+        db.close()
+
+
 @app.post('/blog')
-def create(blog:BlogValidate):
-    return {'Title':blog.title , 'Body':blog.body}
+def create(blog:BlogValidate,db:Session=Depends(get_db)):
+    new_blog = Blog(title=blog.title,body=blog.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+    #return {'Title':blog.title , 'Body':blog.body}
